c52eea3d54066b4a1af4b0164f6be67550e89a15
EviBera <beraevi@gmail.com>
2024-05-29 11:29:21 +0200

    Implement IProduct interface

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -4,9 +4,10 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   template: `
   <div>
+    <h5>My first component (AppComponent - whole screen, its title:)</h5>
     <h1>{{pageTitle}}</h1>
-    <h5>My first component (up)</h5>
-    <h5>My second component (down)</h5>
+    <hr style="opacity:1;height:5px;background:black;">
+    <h5>My second component (ProductListComponent - below the line)</h5>
     <pm-products></pm-products>
   </div>
   `
