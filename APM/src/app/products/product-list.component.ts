31371194d8f8972ad10a8ff2745af616da9a0ea6
EviBera <beraevi@gmail.com>
2024-05-29 15:43:33 +0200

    Add getter and setter to handle filtering

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter: string = '';
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      console.log('In setter: ', value);
+    }
+
     products: IProduct[] = [
         {
           "productId": 1,
@@ -48,8 +57,8 @@ export class ProductListComponent implements OnInit {
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
-    
+
     ngOnInit(): void {
-      console.log('OnInit');
+      this.listFilter = 'cart';
     }
   }
