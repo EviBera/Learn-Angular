62fd036a22e727f67a6cbaf85e155009643c9c96
EviBera <beraevi@gmail.com>
2024-05-30 10:20:55 +0200

    Pass star rating data to the nested component using @Input decorator

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -6,10 +6,12 @@ import { Component, OnChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    rating: number = 4;
+    @Input() rating: number = 0;
     cropWidth: number = 75;
 
     ngOnChanges(): void {
+        console.log('Current rating:', this.rating);
         this.cropWidth = this.rating * 75/5;
+        console.log('Calculated cropWidth:', this.cropWidth);
     }
 }
