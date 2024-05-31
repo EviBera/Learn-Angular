cae1a4292e461fec1ceaf6c23022791bd0757661
EviBera <beraevi@gmail.com>
2024-05-31 14:46:53 +0200

    Tie routes to actions

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root',
   template: `
-  <div>
     <h5>My first component (AppComponent - whole screen, its title:)</h5>
-    <h1>{{pageTitle}}</h1>
-    <hr style="opacity:1;height:5px;background:black;">
-    <h5>My second component (ProductListComponent - below the line)</h5>
-    <pm-products></pm-products>
-  </div>
+    
+    <nav class='navbar nav-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='nav nav-pills'>
+        <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+        <li><a class='nav-link' [routerLink]="['/products']">Products</a></li>
+      </ul>
+    </nav>
   `
 })
 export class AppComponent {
