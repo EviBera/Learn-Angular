7bcbce1fa50619fa05c93bb0416b4d81053ddd9c
EviBera <beraevi@gmail.com>
2024-05-31 10:43:23 +0200

    Implement an observable to retrieve data (send HTTP GET request)

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,50 +1,62 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product list';
   imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
-    private _listFilter: string = '';
-    get listFilter(): string {
-      return this._listFilter;
-    }
-    set listFilter(value: string) {
-      this._listFilter = value;
-      console.log('In setter: ', value);
-      this.filteredProducts = this.performFilter(value);
-    }
-
-    filteredProducts: IProduct[] = [];
-
-    products: IProduct[] = [];
-    
-    constructor(private productService: ProductService) {}
-
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
-
-    ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
-    }
-
-    performFilter(filterBy: string): IProduct[] {
-      filterBy = filterBy.toLowerCase();
-      return this.products.filter((product: IProduct) =>
-      product.productName.toLowerCase().includes(filterBy));
-    }
-
-    onRatingClicked(message: string): void {
-      this.pageTitle = 'Product list: ' + message;
-    }
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
   }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter: ', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+
+  products: IProduct[] = [];
+  
+  constructor(private productService: ProductService) {}
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+  
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLowerCase().includes(filterBy));
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product list: ' + message;
+  }
+}
