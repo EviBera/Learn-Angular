5a5c0694ebd22939745104dc8071a19bd4c494cd
EviBera <beraevi@gmail.com>
2024-06-04 10:56:33 +0200

    Display product details

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,17 +11,28 @@ export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product detail';
   product: IProduct | undefined;
+  errorMessage: string = '';
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += `: ${id}`;
+    if(id){
+      this.getProduct(id);
+    }
   }
 
   onBack(): void {
     this.router.navigate(['/products']);
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
 }
