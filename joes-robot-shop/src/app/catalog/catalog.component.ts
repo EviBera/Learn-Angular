20bf66a51c38f3961bd6fadd1b7e161cad640624
EviBera <beraevi@gmail.com>
2024-06-10 10:21:06 +0200

    Implement interface for product objects and display a single object in CatalogComponent

--- joes-robot-shop/src/app/catalog/catalog.component.ts
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct = {
+    id: 5,
+    description:
+      "A robot head with three oscillating eyes -- excellent for surveillance.",
+    name: "Surveillance",
+    imageName: "head-surveillance.png",
+    category: "Heads",
+    price: 1255.5,
+    discount: 0,
+  }
+
+  //the tutorial uses a constructor and hardcodes this.product to be a chosen robot part:
+/*   
+  constructor() {
+    this.product = {
+      id: 2,
+    description:
+      "A friendly robot head with two eyes and a smile -- great for domestic use.",
+    name: "Friendly Bot",
+    imageName: "head-friendly.png",
+    category: "Heads",
+    price: 945.0,
+    discount: 0.2,
+    }
+  }
+ */
 
 }
