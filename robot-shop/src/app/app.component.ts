e7948b18d8c9ccb8cbacfe17d72e395c38dc1bc7
EviBera <beraevi@gmail.com>
2024-06-12 16:17:24 +0200

    Create empty project

---
+++ robot-shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'robot-shop';
+}
