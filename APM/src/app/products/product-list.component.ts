44dae6b97db6444e2c4d86e194be0aae36f4ffc6
EviBera <beraevi@gmail.com>
2024-06-18 16:12:04 +0200

    Restore deleted folders

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
+
+@Component({
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle: string = 'Product list';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter: ', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+
+  products: IProduct[] = [];
+  
+  constructor(private productService: ProductService) {}
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+  
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLowerCase().includes(filterBy));
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product list: ' + message;
+  }
+}
