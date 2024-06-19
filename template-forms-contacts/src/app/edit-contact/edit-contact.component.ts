b76084751448eddca78358e2eed82213dfb964db
EviBera <beraevi@gmail.com>
2024-06-19 12:42:17 +0200

    Import FormsModule into the standalone EditContactComponent

--- template-forms-contacts/src/app/edit-contact/edit-contact.component.ts
+++ template-forms-contacts/src/app/edit-contact/edit-contact.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
