f261a8971cce6df0452550fb3ebaa5d1a4188265
EviBera <beraevi@gmail.com>
2024-07-19 16:19:27 +0200

    Create a custom validator

---
+++ template-forms-contacts/src/app/validators/restricted-words-validator.directive.ts
@@ -0,0 +1,21 @@
+import { Directive } from "@angular/core";
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
+
+@Directive({
+    selector: '[restrictedWords]',
+    standalone: true,
+    providers: [{
+        provide: NG_VALIDATORS,
+        multi: true,
+        useExisting: RestrictedWordsValidator,
+    }],
+})
+export class RestrictedWordsValidator implements Validator{
+    validate(control: AbstractControl): ValidationErrors | null {
+        return null;
+    }
+    registerOnValidatorChange?(fn: () => void): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
